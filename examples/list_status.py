292dfc6b701a3f1afc76902b26e7afa2fac5e15a
Jonas Nilsson <mjc.nilsson@gmail.com>
2020-10-13 13:54:48 +0200

    Fix example code.

--- examples/list_status.py
+++ examples/list_status.py
@@ -14,20 +14,20 @@ def print_current_state(channel: WorkerCommandChannel):
     print("-" * 80)
     for job in channel.list_known_jobs():
         print("{:30s}{:30s}{:30s}".format(job.service_id, job.job_id, job.state))
-        if len(job.error_message) > 0:
-            print("    Message: {}".format(job.error_message))
+        if len(job.message) > 0:
+            print("    Message: {}".format(job.message))
 
     print("\nKnown commands")
     print("{:30s}{:30s}{:30s}".format("Job id", "Command id", "Current state"))
     print("-" * 80)
     for command in channel.list_known_commands():
         print("{:26}{:26}{:26}".format(command.job_id, command.command_id, command.state))
-        if len(command.error_message) > 0:
-            print("    Message: {}".format(command.error_message))
+        if len(command.message) > 0:
+            print("    Message: {}".format(command.message))
 
 
 if __name__ == "__main__":
     kafka_host = "dmsc-kafka01:9092"
-    command_channel = WorkerCommandChannel("{}/command_topic".format(kafka_host))
-    time.sleep(5)
+    command_channel = WorkerCommandChannel("{}/test_command_topic".format(kafka_host))
+    time.sleep(10)
     print_current_state(command_channel)
