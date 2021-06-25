0a0eeb01689fe0014b8c109f49bf07789b79890e
Jonas Nilsson <mjc.nilsson@gmail.com>
2021-06-25 11:02:16 +0200

    Better imports.

---
+++ file_writer_control/__init__.py
@@ -0,0 +1,7 @@
+from file_writer_control.JobHandler import JobHandler
+from file_writer_control.WorkerCommandChannel import WorkerCommandChannel
+from file_writer_control.WorkerJobPool import WorkerJobPool
+from file_writer_control.WriteJob import WriteJob
+from file_writer_control.CommandStatus import CommandState
+from file_writer_control.JobStatus import JobState
+
