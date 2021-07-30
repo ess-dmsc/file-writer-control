b160252b0de22973cb661bd368bf6593fb6840ac
Kenan Muric <kenan.muric@ess.eu>
2021-07-30 14:08:00 +0200

    updating to aborting fw job instead of stop job now

--- file_writer_control/WorkerFinder.py
+++ file_writer_control/WorkerFinder.py
@@ -68,15 +68,15 @@ class WorkerFinderBase:
         self.send_command(message)
         return CommandHandler(self.command_channel, command_id)
 
-    def try_send_stop_now(self, service_id: str, job_id: str) -> CommandHandler:
+    def try_send_abort(self, service_id: str, job_id: str) -> CommandHandler:
         """
-        Sends a "stop now" message to a file-writer running a job as identified by the parameters of this function.
+        Sends a "abort" message to a file-writer running a job as identified by the parameters of this function.
         This function is not blocking. No guarantees are given that this command will be followed.
         :param service_id: The service identifier of the file-writer to receive the command.
         :param job_id: The job identifier of the currently running file-writer job.
-        :return: A CommandHandler instance for (more) easily checking the outcome of the "stop now" command.
+        :return: A CommandHandler instance for (more) easily checking the outcome of the "abort" command.
         """
-        return self.try_send_stop_time(service_id, job_id, datetime.now())
+        return self.try_send_stop_time(service_id, job_id, 0)
 
     def list_known_workers(self) -> List[WorkerStatus]:
         """
